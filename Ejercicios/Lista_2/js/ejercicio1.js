e9ad90d3fcb9aef5450c9f9af3a667ae6207e618
crushcrushcrush99 <rockletsamor@gmail.com>
2023-04-05 23:00:09 -0300

    subiendo aulas y ejercicios

---
+++ Ejercicios/Lista_2/js/ejercicio1.js
@@ -0,0 +1,25 @@
+var marisaSalario = 3000
+var lautaroSalario = 1500
+var soledadSalario = 1000
+var melinaSalario = 950
+var rodolfoSalario = 2500
+
+var reajusteSalarial= 10
+
+var valorReajusteTrabajador1 = marisaSalario * 10 / 100
+var valorReajusteTrabajador2 = lautaroSalario * 10 / 100
+var valorReajusteTrabajador3 = soledadSalario * 10 / 100
+var valorReajusteTrabajador4 = melinaSalario * 10 / 100
+var valorReajusteTrabajador5 = rodolfoSalario * 10 / 100
+
+var salarioFinal1 = marisaSalario + valorReajusteTrabajador1
+var salarioFinal2 = lautaroSalario + valorReajusteTrabajador2
+var salarioFinal3 = soledadSalario + valorReajusteTrabajador3
+var salarioFinal4 = melinaSalario + valorReajusteTrabajador4
+var salarioFinal5 = rodolfoSalario + valorReajusteTrabajador5
+
+console.log('Marisa gana ' + 'R$'+ marisaSalario + ', con reajuste de 10% pasa a ganar ' + 'R$' + salarioFinal1)
+console.log('Lautaro gana ' + 'R$'+ lautaroSalario + ', con reajuste de 10% pasa a ganar ' + 'R$' + salarioFinal2)
+console.log('Soledad gana ' + 'R$'+ soledadSalario + ', con reajuste de 10% pasa a ganar ' + 'R$' + salarioFinal3)
+console.log('Melina gana ' + 'R$'+ melinaSalario + ', con reajuste de 10% pasa a ganar ' + 'R$' + salarioFinal4)
+console.log('Rodolfo gana ' + 'R$'+ rodolfoSalario + ', con reajuste de 10% pasa a ganar ' + 'R$' + salarioFinal5)
